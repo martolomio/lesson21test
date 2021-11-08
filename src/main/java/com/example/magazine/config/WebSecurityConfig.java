3cf2c9eb3025b1beeea02f0f29b3590b8a404b7b
martha <partutamarta@gmail.com>
2021-11-08 23:13:04 +0200

    60:00

--- src/main/java/com/example/magazine/config/WebSecurityConfig.java
+++ src/main/java/com/example/magazine/config/WebSecurityConfig.java
@@ -1,6 +1,43 @@
 package com.example.magazine.config;
 
+import com.example.magazine.security.CustomUserDetails;
+import com.example.magazine.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = CustomUserDetailsService.class)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    public void configAuthentication( AuthenticationManagerBuilder amb) throws Exception {
+        amb.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+    @Bean(name = "passwordEncoder")
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/home").access("hasRole('ROLE_USER')").anyRequest().permitAll().and()
+
+                .formLogin().loginPage("/login")
+                .defaultSuccessUrl("/home").usernameParameter("email").passwordParameter("password").and()
+                .logout().logoutSuccessUrl("/login?logout").and()
+                .exceptionHandling().accessDeniedPage("/403").and()
+                .csrf();
+    }
 }
